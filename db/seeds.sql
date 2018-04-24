3644e46c441135f1258d944eaf1c1127bac933fb
asterrypm <asterrypm@gmail.com>
2018-04-24 12:34:27 -0400

    infrastructure

---
+++ db/seeds.sql
@@ -0,0 +1,56 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smoke and Pepper');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black and Bleu');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Madlove Burger');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Southern Charm Burger');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Marco Polo');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Red Robin Gourmet Cheeseburger');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sauteed Shroom');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whiskey River BBQ');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Chili Cheeseburger');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royal Red Robin');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('A.I. Peppercorn');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bleu Ribbon');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Prime Chophouse');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Guacamole Bacon');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Banzai');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burning Love');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Taco Tavern Double');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sir Acha Tavern Double');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smokey Jack Tavern Double');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Reds Tavern Double');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pig Out Tavern Double');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fiery Ghost Tavern Double');
+
+
+
+
+
+
+
+
+
