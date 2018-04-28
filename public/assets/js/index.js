591ad9c1ca2d353fb8240ea38667c464672a8966
asterrypm <asterrypm@gmail.com>
2018-04-28 19:15:30 -0400

    index.js error

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,44 +1,44 @@
-
 // On ready
-$(function() {
+$(function () {
   // Handle Post Requests For new burgers
-  $('#add-burger').on("click", function() {
+  $('#add-burger').on("click", function () {
     event.preventDefault();
     // make an object with that burger's name inside of it
-  var newBurger = {
-    burger_name: $("#burger-name").val().trim()
-  }
-  };
-    // do a post request with jquery
-  $.ajax("/api/burgers/add", {
+    var newBurger = {
+      burger_name: $("#burger-name").val().trim(),
+      devoured: false
+    }
+  
+  // do a post request with jquery
+  $.ajax("/api/burgers/", {
     type: "POST",
     data: newBurger
-  }).then (
-    function() {
+  }).then(
+    function () {
       console.log("New Burger added");
       location.reload();
     }
-  });
+  );
+});
 
-  // Handle PUT request for devoured burgers
-  $('.js-devour-handler').on("click", function(event) {
-    event.preventDefault();
-    // get the data-id attributes value from the button
-    var id = $(this).data("id");
+// Handle PUT request for devoured burgers
+$('.js-devour-handler').on("click", function (event) {
+  event.preventDefault();
+  // get the data-id attributes value from the button
+  var id = $(this).data("id");
 
-    // compose an object with the id inside of it
-    var devoured = {
-      devoured: true
-    };
-    // do a put request with jquery
-    $.ajax("/api/burgers/devoured/:id", {
-      type: "PUT",
-      data: devoured
-    }).then(
-      function() {
-        console.log("devoured burger");
-        location.reload();
-      }
-
-  });
- });
+  // compose an object with the id inside of it
+  var devoured = {
+    devoured: true
+  };
+  // do a put request with jquery
+  $.ajax("/api/burgers/devoured/:id", {
+    type: "PUT",
+    data: devoured
+  }).then(
+    function () {
+      console.log("devoured burger");
+      location.reload();
+    }
+  );
+});
