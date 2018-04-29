9d5d1d0e391ca0c32144f65ae0673248e4990ea1
asterrypm <asterrypm@gmail.com>
2018-04-28 20:28:56 -0400

    solved index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,44 +1,38 @@
-// On ready
 $(function () {
-  // Handle Post Requests For new burgers
+
   $('#add-burger').on("click", function () {
     event.preventDefault();
-    // make an object with that burger's name inside of it
     var newBurger = {
       burger_name: $("#burger-name").val().trim(),
       devoured: false
-    }
-  
-  // do a post request with jquery
-  $.ajax("/api/burgers/", {
-    type: "POST",
-    data: newBurger
-  }).then(
-    function () {
-      console.log("New Burger added");
-      location.reload();
-    }
-  );
-});
+    };
 
-// Handle PUT request for devoured burgers
-$('.js-devour-handler').on("click", function (event) {
-  event.preventDefault();
-  // get the data-id attributes value from the button
-  var id = $(this).data("id");
+    $.ajax("/api/burgers/", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function () {
+        console.log("New Burger added");
+        location.reload();
+      }
+    );
+  });
 
-  // compose an object with the id inside of it
-  var devoured = {
-    devoured: true
-  };
-  // do a put request with jquery
-  $.ajax("/api/burgers/devoured/:id", {
-    type: "PUT",
-    data: devoured
-  }).then(
-    function () {
-      console.log("devoured burger");
-      location.reload();
-    }
-  );
+  $('.js-devour-handler').on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).data("id");
+    var devoured = {
+      devoured: true
+    };
+
+    $.ajax("/api/burgers/devoured/:id", {
+      type: "PUT",
+      data: devoured
+    }).then(
+      function () {
+        console.log("devoured burger");
+        location.reload();
+      }
+    );
+  });
 });
